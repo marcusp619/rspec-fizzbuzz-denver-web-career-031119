22e90e2adb95738b53eae02571c9ea297213f454
Mark Pearyer <marcusp619@gmail.com>
2019-04-26 16:34:33 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 
 def fizzbuzz(num)
-  if num % 3 == 0
+  if num % 3 == 0 && num % 5 != 0
     'Fizz'
   elsif num % 5 == 0 && num % 3 != 0 || num % 3 == 0 && num % 5 != 0
     'Buzz'
   elsif num % 3 == 0 && num % 5 == 0
     'FizzBuzz'
   end
-end
+end 
